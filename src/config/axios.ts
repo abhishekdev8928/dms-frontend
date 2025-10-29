2d6deccf286492df709ce06d588ec56a1fa70e61
abhishek62682 <abhishekreact.dev@gmail.com>
2025-10-29 11:40:47 +0530

    v2

--- src/config/axios.ts
+++ src/config/axios.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 import { useAuthStore } from "./store/useAuthStore"; // Adjust the import path for your store
 
 const api = axios.create({
-  baseURL: "https://dms-backend-3.onrender.com/api", // Your base URL
+  baseURL: "https://dmsdev.run.place/api", // Your base URL
   headers: {
     "Content-Type": "application/json", // Standard content type
   },
